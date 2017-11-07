eb9e03e409a0761609f32bd0c7ce2f0f50e28957
Peter Omara <peter.omara@okstate.edu>
2017-11-06 20:42:06 -0600

    I have added some logical computations

--- Justforpractice.R
+++ Justforpractice.R
@@ -3,3 +3,25 @@ x<-41
 y<-23
 z<-12
 print(x+y+z)
+#1--1
+
+
+if(x<10) {
+  print(FALSE)
+}else {
+  print(TRUE)
+}
+
+
+if(x+z<10) {
+  print(FALSE)
+}else {
+  print(TRUE)
+}
+
+if(y+x<100) {
+  print(TRUE)
+} else {
+  print(FALSE)
+}
+
